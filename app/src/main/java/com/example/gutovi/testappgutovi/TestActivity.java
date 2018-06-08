b2f3924209f6e3d582f6bc5b8f78c1b45c894b91
Gutovi <gutovi.kun@gmail.com>
2018-06-07 21:06:36 -0300

    New images and other details.

--- app/src/main/java/com/example/gutovi/testappgutovi/TestActivity.java
+++ app/src/main/java/com/example/gutovi/testappgutovi/TestActivity.java
@@ -3,9 +3,12 @@ package com.example.gutovi.testappgutovi;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.transition.Visibility;
 import android.view.View;
 
 public class TestActivity extends AppCompatActivity {
+
+    boolean surveyOn = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,6 +33,15 @@ public class TestActivity extends AppCompatActivity {
                 intent.putExtra(Intent.EXTRA_TEXT, "Este es un mail de prueba enviado desde "+ getResources().getString(R.string.app_name));
                 Intent mailappchooser = Intent.createChooser(intent, "¿Con qué enviar este e-mail?");
                 startActivity(mailappchooser);
+            }
+        });
+
+
+        findViewById(R.id.btn_survey).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                surveyOn = !surveyOn;
+                findViewById(R.id.rdg_Survey).setVisibility (View.VISIBLE);
             }
         });
     }
--- app/src/main/java/com/example/gutovi/testappgutovi/TestLogin.java
+++ app/src/main/java/com/example/gutovi/testappgutovi/TestLogin.java
@@ -32,22 +32,12 @@ import android.widget.TextView;
 import java.util.ArrayList;
 import java.util.List;
 
-import static android.Manifest.permission.READ_CONTACTS;
 
 /**
  * A login screen that offers login via email/password.
  */
 public class TestLogin extends AppCompatActivity implements LoaderCallbacks<Cursor> {
 
-    /**
-     * Id to identity READ_CONTACTS permission request.
-     */
-    private static final int REQUEST_READ_CONTACTS = 0;
-
-    /**
-     * A dummy authentication store containing known user names and passwords.
-     * TODO: remove after connecting to a real authentication system.
-     */
     private static final String[] DUMMY_CREDENTIALS = new String[]{
             "foo@example.com:hello", "bar@example.com:world"
     };
@@ -68,7 +58,6 @@ public class TestLogin extends AppCompatActivity implements LoaderCallbacks<Curs
         setContentView(R.layout.activity_test_login);
         // Set up the login form.
         mEmailView = (AutoCompleteTextView) findViewById(R.id.email);
-        populateAutoComplete();
 
         mPasswordView = (EditText) findViewById(R.id.password);
         mPasswordView.setOnEditorActionListener(new TextView.OnEditorActionListener() {
@@ -94,48 +83,7 @@ public class TestLogin extends AppCompatActivity implements LoaderCallbacks<Curs
         mProgressView = findViewById(R.id.login_progress);
     }
 
-    private void populateAutoComplete() {
-        if (!mayRequestContacts()) {
-            return;
-        }
 
-        getLoaderManager().initLoader(0, null, this);
-    }
-
-    private boolean mayRequestContacts() {
-        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.M) {
-            return true;
-        }
-        if (checkSelfPermission(READ_CONTACTS) == PackageManager.PERMISSION_GRANTED) {
-            return true;
-        }
-        if (shouldShowRequestPermissionRationale(READ_CONTACTS)) {
-            Snackbar.make(mEmailView, R.string.permission_rationale, Snackbar.LENGTH_INDEFINITE)
-                    .setAction(android.R.string.ok, new View.OnClickListener() {
-                        @Override
-                        @TargetApi(Build.VERSION_CODES.M)
-                        public void onClick(View v) {
-                            requestPermissions(new String[]{READ_CONTACTS}, REQUEST_READ_CONTACTS);
-                        }
-                    });
-        } else {
-            requestPermissions(new String[]{READ_CONTACTS}, REQUEST_READ_CONTACTS);
-        }
-        return false;
-    }
-
-    /**
-     * Callback received when a permissions request has been completed.
-     */
-    @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions,
-                                           @NonNull int[] grantResults) {
-        if (requestCode == REQUEST_READ_CONTACTS) {
-            if (grantResults.length == 1 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                populateAutoComplete();
-            }
-        }
-    }
 
 
     /**
